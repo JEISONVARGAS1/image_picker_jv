2e4a82a9b1318b468713ce5939e0128da646de19
jeisonVargas <jeison1vargas@gmail.com>
2022-04-15 14:04:06 -0500

    agregando validaciones

--- lib/atoms/circular_frame.dart
+++ lib/atoms/circular_frame.dart
@@ -1,8 +1,6 @@
 /*import 'package:shimmer/shimmer.dart';*/
 import 'package:flutter/material.dart';
-import 'package:image_picker_jv/tokens/app_color.dart';
 import 'package:image_picker_jv/organisms/frame_image.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:image_picker_jv/tokens/generate_provider_image.dart';
 
 class CircularFrame extends StatelessWidget {
@@ -31,7 +29,7 @@ class CircularFrame extends StatelessWidget {
       case ImageProviderFrame.assets:
         return _generateSimpleProvider();
       case ImageProviderFrame.network:
-        return _generateCacheProvider();
+        return _generateSimpleProvider();
     }
   }
 
@@ -51,30 +49,7 @@ class CircularFrame extends StatelessWidget {
   }
 
   Widget _generateCacheProvider() {
-    return CachedNetworkImage(
-      imageUrl: image,
-      imageBuilder: (context, cacheImageProvider) {
-        return Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            color: color,
-            shape: BoxShape.circle,
-            image: generateProviderImage(
-              image: image,
-              imageProvider: imageProvider,
-            ),
-          ),
-        );
-      },
-      placeholder: (context, url) {
-        return Container();
-      },
-      errorWidget: (context, url, error) => const Icon(
-        Icons.error,
-        color: AppColor.orange,
-      ),
-    ); /*Shimmer.fromColors(
+    return Container(); /*Shimmer.fromColors(
       baseColor: AppColor.gray,
       highlightColor: AppColor.white,
       child: Container(
@@ -84,6 +59,39 @@ class CircularFrame extends StatelessWidget {
             color: color,
             shape: BoxShape.circle
         ),
+      ),
+    );*/ /*CachedNetworkImage(
+      imageUrl: image,
+      imageBuilder: (context, cacheImageProvider) => Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          image: generateProviderImage(
+            image: image,
+            imageProvider: imageProvider,
+            cacheImageProvider: cacheImageProvider,
+          ),
+        ),
+      ),
+      placeholder: (context, url) {
+        return Shimmer.fromColors(
+          baseColor: AppColor.gray,
+          highlightColor: AppColor.white,
+          child: Container(
+            width: size,
+            height: size,
+            decoration: BoxDecoration(
+                color: color,
+                shape: BoxShape.circle
+            ),
+          ),
+        );
+      },
+      errorWidget: (context, url, error) => const Icon(
+        Icons.error,
+        color: AppColor.orange,
       ),
     );*/
   }
